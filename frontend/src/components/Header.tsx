42020e9202408ab44b22169531055340b8fad69a
Pavel <magma4810@gmail.com>
2025-04-17 18:45:07 +0300

    fix

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,7 +18,7 @@ export const Header: FC = () => {
       <div className="flex items-center justify-center">
         <Icon href="/" src={muscle} />
         <a href="/">
-          <span className="text-3xl">SportFuelMarket004s</span>
+          <span className="text-3xl">SportFuelMarket00224s</span>
         </a>
       </div>
       {isAuthenticated && (
