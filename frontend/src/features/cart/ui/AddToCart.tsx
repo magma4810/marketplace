23a1a71f204167c3feddf7e8d96a1df3c94fb4f5
Pavel <magma4810@gmail.com>
2025-05-26 21:45:15 +0300

    AddToCartButton

--- frontend/src/features/cart/ui/AddToCart.tsx
+++ frontend/src/features/cart/ui/AddToCart.tsx
@@ -5,6 +5,20 @@ import { useSelector } from "react-redux";
 import { Products } from "../../../../types";
 import { addProductsID } from "@/app/providers/store/user.slice";
 import { StoreApp,useAppDispatch } from "@/app/providers/store";
+import styled from "styled-components";
+
+const AddToCartButton = styled.button`
+  cursor: pointer;
+  background-color: rgba(254, 243, 199, 0.5);
+  border-radius: 9999px; 
+  width: 70%;
+  height: 50%;
+  transition: background-color 0.2s ease-in-out;
+  
+  &:hover {
+    background-color: rgba(253, 230, 138, 0.5);
+  }
+`;
 
 export const AddToCart: FC<{ data: Products }> = ({ data }) => {
   const isAuthenticated = useSelector(
@@ -20,16 +34,15 @@ export const AddToCart: FC<{ data: Products }> = ({ data }) => {
       {isAuthenticated && count > 0 ? (
         <Counter id={data.id} />
       ) : (
-        <button
+        <AddToCartButton
           onClick={() =>
             isAuthenticated
               ? dispatch(addProductsID(data.id))
               : navigate("/signin")
           }
-          className="cursor-pointer bg-amber-100/50 rounded-4xl w-[70%] h-[50%] hover:bg-amber-200/50 transition-colors"
         >
           Добавить в корзину
-        </button>
+        </AddToCartButton>
       )}
     </>
   );
